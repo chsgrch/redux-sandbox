3c3842b1fdc7124c67ba816a30c1679c2e2847c6
Sergey Chuyan <chsgrch@gmail.com>
2020-10-22 21:17:11 +0300

    Add connect from 'react-redux' add connect for return HOC component with component Counter for send in this component state and bind actions

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,9 @@
 // --- Action creator ---
-export const setInc = () => ({ type: 'INC' })
-export const setDec = () => ({ type: 'DEC' })
-export const setRnd = (rndValue) => ({ type: 'RND', rndValue })
+export const setInc = () => ({ type: "INC" });
+export const setDec = () => ({ type: "DEC" });
+export const setRnd = () => {
+  return {
+    type: "RND",
+    rndValue: Math.floor(Math.random() * 10),
+  };
+};
